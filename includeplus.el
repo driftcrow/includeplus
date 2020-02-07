b1a98cf9f8082c110e5c1797b7a33666ec97ae22
driftcrow <driftcrow@gmail.com>
2020-02-07 22:48:50 +0800

    update some  ip  and ipp funcs

--- includeplus.el
+++ includeplus.el
@@ -1,8 +1,14 @@
 ;;; includeplus.el -*- lexical-binding: t; -*-
 ;;; Code:
 ;;;
-(defvar ip-plus-re "^[ \t]*#\\+eval:"
+(require 'includeplusplus)
+
+(defvar ip-plus-re "^[ \t]*#\\+plus:"
   "Include plus block main symbol regexp.")
+(defvar ip-cache-director "./.plus"
+  "Include plus create cache file directory.")
+(defvar ip-cache-file-list '()
+  "All cache file list for clean interval.")
 
 (defun ip-update-include-plus (&optional included dir footnotes)
   "update every include keyword plus in buffer.
@@ -13,134 +19,45 @@ working directory.  It is used to properly resolve relative
 paths.  Optional argument FOOTNOTES is a hash-table used for
 storing and resolving footnotes.  It is created automatically."
   (let ((includer-file (buffer-file-name (buffer-base-buffer)))
-	(case-fold-search t)
-	(file-prefix (make-hash-table :test #'equal))
-	(current-prefix 0)
-	(footnotes (or footnotes (make-hash-table :test #'equal)))
-	(include-re "^[ \t]*#\\+INCLUDE:"))
+        (case-fold-search t)
+        (include-re "^[ \t]*#\\+INCLUDE:"))
     ;; Expand INCLUDE keywords.
     (goto-char (point-min))
     (while (re-search-forward include-re nil t)
       (when (and (not (org-in-commented-heading-p))
                  (ip-is-plus-p))
-	(let ((element (save-match-data (org-element-at-point))))
-	  (when (eq (org-element-type element) 'keyword)
-	    (beginning-of-line)
-	    ;; Extract arguments from keyword's value.
-	    (let* ((value (org-element-property :value element))
-		   (ind (current-indentation))
-		   location
-		   (coding-system-for-read
-		    (or (and (string-match ":coding +\\(\\S-+\\)>" value)
-			     (prog1 (intern (match-string 1 value))
-			       (setq value (replace-match "" nil nil value))))
-			coding-system-for-read))
-		   (file
-		    (and (string-match "^\\(\".+?\"\\|\\S-+\\)\\(?:\\s-+\\|$\\)"
-				       value)
-			 (prog1
-			     (save-match-data
-			       (let ((matched (match-string 1 value)))
-				 (when (string-match "\\(::\\(.*?\\)\\)\"?\\'"
-						     matched)
-				   (setq location (match-string 2 matched))
-				   (setq matched
-					 (replace-match "" nil nil matched 1)))
-				 (expand-file-name (org-strip-quotes matched)
-						   dir)))
-			   (setq value (replace-match "" nil nil value)))))
-		   (only-contents
-		    (and (string-match ":only-contents *\\([^: \r\t\n]\\S-*\\)?"
-				       value)
-			 (prog1 (org-not-nil (match-string 1 value))
-			   (setq value (replace-match "" nil nil value)))))
-		   (lines
-		    (and (string-match
-			  ":lines +\"\\([0-9]*-[0-9]*\\)\""
-			  value)
-			 (prog1 (match-string 1 value)
-			   (setq value (replace-match "" nil nil value)))))
-		   (env (cond
-			 ((string-match "\\<example\\>" value) 'literal)
-			 ((string-match "\\<export\\(?: +\\(.*\\)\\)?" value)
-			  'literal)
-			 ((string-match "\\<src\\(?: +\\(.*\\)\\)?" value)
-			  'literal)))
-		   ;; Minimal level of included file defaults to the
-		   ;; child level of the current headline, if any, or
-		   ;; one.  It only applies is the file is meant to be
-		   ;; included as an Org one.
-		   (minlevel
-		    (and (not env)
-			 (if (string-match ":minlevel +\\([0-9]+\\)" value)
-			     (prog1 (string-to-number (match-string 1 value))
-			       (setq value (replace-match "" nil nil value)))
-			   (get-text-property (point)
-					      :org-include-induced-level))))
-		   (args (and (eq env 'literal) (match-string 1 value)))
-		   (block (and (string-match "\\<\\(\\S-+\\)\\>" value)
-			       (match-string 1 value))))
-	      ;; Remove keyword.
-	      (delete-region (point) (line-beginning-position 2))
-	      (cond
-	       ((not file) nil)
-	       ((not (file-readable-p file))
-		(error "Cannot include file %s" file))
-	       ;; Check if files has already been parsed.  Look after
-	       ;; inclusion lines too, as different parts of the same
-	       ;; file can be included too.
-	       ((member (list file lines) included)
-		(error "Recursive file inclusion: %s" file))
-	       (t
-		(cond
-		 ((eq env 'literal)
-		  (insert
-		   (let ((ind-str (make-string ind ?\s))
-			 (arg-str (if (stringp args) (format " %s" args) ""))
-			 (contents
-			  (org-escape-code-in-string
-			   (org-export--prepare-file-contents file lines))))
-		     (format "%s#+BEGIN_%s%s\n%s%s#+END_%s\n"
-			     ind-str block arg-str contents ind-str block))))
-		 ((stringp block)
-		  (insert
-		   (let ((ind-str (make-string ind ?\s))
-			 (contents
-			  (org-export--prepare-file-contents file lines)))
-		     (format "%s#+BEGIN_%s\n%s%s#+END_%s\n"
-			     ind-str block contents ind-str block))))
-		 (t
-		  (insert
-		   (with-temp-buffer
-		     (let ((org-inhibit-startup t)
-			   (lines
-			    (if location
-				(org-export--inclusion-absolute-lines
-				 file location only-contents lines)
-			      lines)))
-		       (org-mode)
-		       (insert
-			(org-export--prepare-file-contents
-			 file lines ind minlevel
-			 (or (gethash file file-prefix)
-			     (puthash file
-				      (cl-incf current-prefix)
-				      file-prefix))
-			 footnotes
-			 includer-file)))
-		     (org-export-expand-include-keyword
-		      (cons (list file lines) included)
-		      (file-name-directory file)
-		      footnotes)
-		     (buffer-string)))))
-		;; Expand footnotes after all files have been
-		;; included.  Footnotes are stored at end of buffer.
-		(unless included
-		  (org-with-wide-buffer
-		   (goto-char (point-max))
-		   (maphash (lambda (k v)
-			      (insert (format "\n[fn:%s] %s\n" k v)))
-			    footnotes))))))))))))
+        (let ((element (save-match-data (org-element-at-point)))
+              (plus (save-match-data (ip-get-eval))))
+          (when (eq (org-element-type element) 'keyword)
+            (beginning-of-line)
+            ;; Extract arguments from keyword's value.
+            (let* ((value (org-element-property :value element))
+                   location
+                   (file
+                    (and (string-match "^\\(\".*?\"\\|\\S-+\\)\\(?:\\s-+\\|$\\)"
+                                       value)
+                         (prog1
+                             (save-match-data
+                               (let ((matched (match-string 1 value)))
+                                 (when (string-match "\\(::\\(.*?\\)\\)\"?\\'"
+                                                     matched)
+                                   (setq location (match-string 2 matched))
+                                   (setq matched
+                                         (replace-match "" nil nil matched 1)))
+                                 (expand-file-name (org-strip-quotes matched)
+                                                   dir)))
+                           )))
+                   (plus-file (ip-create-plus-file plus file))
+                   )
+
+              ;; update plus file
+              (when plus-file
+                (if (looking-at  "^[ \t]*#\\+INCLUDE: *\\(\".*?\"\\|\\S-+\\)\\(?:\\s-+\\|$\\)")
+                    (replace-match (concat " \"" plus-file (if location (concat "::" location) "") "\"") :fixedcase :literal nil 1))
+                )
+              ;; TODO: remove plus keyword
+             
+              )))))))
 
 (defun ip-preview ()
   "Preview current include block contents."
@@ -160,26 +77,11 @@ storing and resolving footnotes.  It is created automatically."
 (defun ip-eval-string (string)
   (eval (car (read-from-string (format "(progn %s)" string)))))
 
-(defun org-in-commented-heading-p (&optional no-inheritance)
-  "Non-nil if point is under a commented heading.
-This function also checks ancestors of the current headline,
-unless optional argument NO-INHERITANCE is non-nil."
-  (cond
-   ((org-before-first-heading-p) nil)
-   ((let ((headline (nth 4 (org-heading-components))))
-      (and headline
-	   (let ((case-fold-search nil))
-	     (string-match-p (concat "^" org-comment-string "\\(?: \\|$\\)")
-			     headline)))))
-   (no-inheritance nil)
-   (t
-    (save-excursion (and (org-up-heading-safe) (org-in-commented-heading-p))))))
-
 (defun ip-is-plus-p ()
-"Non-nil if include block with plus eval. "
-(save-excursion
-  (beginning-of-line 0)
-  (looking-at-p ip-plus-re))
+  "Non-nil if include block with plus eval. "
+  (save-excursion
+    (beginning-of-line 0)
+    (looking-at-p ip-plus-re))
   )
 
 (defun ip-get-eval ()
@@ -195,5 +97,45 @@ unless optional argument NO-INHERITANCE is non-nil."
             (setq funcs (concat value funcs )))))
       funcs)
     ))
+
+(defun ip-create-plus-file (plus &optional file )
+  "Create plus cache file in current dir or same dir with file."
+  (let* ((plus-file (if file
+                        (ip-make-temp-file ".ip-cache-" (file-name-directory file))
+                      (ip-make-temp-file ".ip-cache-"))))
+    (with-temp-file plus-file
+      ;; insert src file contents
+      (cond
+       ((or (not file) (file-directory-p file)) nil)
+       ((not (file-readable-p file))
+        (error "Cannot read src file %s" file))
+       (t
+        (insert-file-contents file)
+        ))
+
+      (if (stringp plus)
+          (ip-eval-string plus))
+
+      plus-file))
+  )
+
+(defun ip-make-temp-file (prefix &optional in-dir dir-flag suffix text)
+  "Create a include plus temporary file."
+  (let ((absolute-prefix
+         (if in-dir
+             (expand-file-name prefix in-dir)
+           (expand-file-name prefix "./"))))
+    (if (find-file-name-handler absolute-prefix 'write-region)
+        (files--make-magic-temp-file absolute-prefix dir-flag suffix text)
+      (make-temp-file-internal absolute-prefix
+                               (if dir-flag t) (or suffix "") text))))
+
+(defun ip-cache-clean ()
+  "Clean ip cache file interval."
+  )
+
+
+(advice-add 'org-export-expand-include-keyword :before #'ip-update-include-plus)
+
 (provide 'includeplus)
 ;;; includeplus.el ends here
--- includeplusplus.el
+++ includeplusplus.el
@@ -1,8 +1,68 @@
 ;;; includeplusplus.el -*- lexical-binding: t; -*-
 
-(defun ipp-table-from-excel ()
-  )
+(defun ip--get-url (url)
+  "Return content for URL as string.
+This uses `url-retrieve-synchronously' to make a request with the
+URL, then returns the response body.  Since that function returns
+the entire response, including headers, we must remove the
+headers ourselves."
+  (let* ((response-buffer (url-retrieve-synchronously url nil t))
+         (encoded-html (with-current-buffer response-buffer
+                         ;; Skip HTTP headers.
+                         ;; FIXME: Byte-compiling says that `url-http-end-of-headers' is a free
+                         ;; variable, which seems to be because it's not declared by url.el with
+                         ;; `defvar'.  Yet this seems to work fine...
+                         (delete-region (point-min) url-http-end-of-headers)
+                         (buffer-string))))
+    ;; NOTE: Be careful to kill the buffer, because `url' doesn't close it automatically.
+    (kill-buffer response-buffer)
+    (with-temp-buffer
+      ;; For some reason, running `decode-coding-region' in the
+      ;; response buffer has no effect, so we have to do it in a
+      ;; temp buffer.
+      (insert encoded-html)
+      (condition-case nil
+          ;; Fix undecoded text
+          (decode-coding-region (point-min) (point-max) 'utf-8)
+        (coding-system-error nil))
+      (buffer-string))))
 
-(defun ipp-text-from-doc ()
-  )
+(defun ipp-from-shell (cmd &optional exp)
+    "Run shell command then inport stdout."
+    (let* ((data (shell-command-to-string cmd))
+         (text (with-temp-buffer
+                  (insert data)
+                  (goto-char (point-min))
+                  (if exp (ip-eval-string exp))
+                  (buffer-string))))
 
+    (insert text)
+    ))
+
+(defun ipp-from-excel (file &optional sheet with-title)
+  "Convert excel xls file to csv for import to org mode."
+  (let* ((cmd (concat "xlsx2csv " file))
+         (data (shell-command-to-string cmd))
+         (table (with-temp-buffer
+                  (insert data)
+                  (org-table-convert-region (point-min) (point-max))
+                  (if with-title
+                      (org-table-insert-hline))
+                  (buffer-string))))
+
+    (insert table)
+    ))
+
+
+  (defun ipp-from-word (file)
+    "Convert word docx file to plain text."
+    (let* ((cmd (concat "docx2txt " file))
+         (data (shell-command-to-string cmd))
+         (text (with-temp-buffer
+                  (insert data)
+                  (buffer-string))))
+
+    (insert text)
+    ))
+
+(provide 'includeplusplus)
